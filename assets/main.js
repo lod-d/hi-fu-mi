ec2c95394ae83205dcd7bc3c8759a542a7226a67
pook mini <mindhox@proton.me>
2025-07-06 16:18:45 +0200

    partie refacto.

--- assets/main.js
+++ assets/main.js
@@ -58,10 +58,8 @@ ciseaux.addEventListener("click", () => {
 const resultat = (choixJoueur) => {    
     const bot = choixBot();
 
-    // affichage du resultat dans le modal 
-    // on ecrit le html du modal 
-    // on met le choix du joueur et du bot 
-    // ainsi que le resultat de la partie 
+    // affichage du resultat dans le modal, on ecrit le html du modal 
+    // on met le choix du joueur et du bot ainsi que le resultat de la partie 
     // on affiche le resultat dans le p avec l'id result
     resultModal.innerHTML = `
     <div class='modal'>
@@ -71,3 +69,53 @@ const resultat = (choixJoueur) => {
     </div>
     `
 }
+
+
+/* PARTIE REFACTORISE */
+
+/*
+
+//  partie refactorée avec un tableau pour le choix 
+const choix = ['pierre', 'papier', 'ciseaux'];
+
+// écoute d'evenement sur les images du joueur
+papier.addEventListener('click', () => jouer('papier'));
+pierre.addEventListener('click', () => jouer('pierre'));
+ciseaux.addEventListener('click', () => jouer('ciseaux'));
+
+//  fonction qui affiche le resultat de la partie 
+//  dans le modal et qui prend en parametre 
+//  le choix du joueur et le choix du bot
+const resultat = ([choixJoueur, choixBot, gagnant]) => {
+    resultModal.innerHTML = `
+    <div class='modal'>
+        <p>Joueur a choisi <span>${choixJoueur}</span> et le bot a choisi <span>${choixBot}</span></p>
+        <p id='result'>${gagnant}</p>
+        <a href="">Une autre ? </a>
+    </div>
+    `
+}
+
+// fonction qui prend en parametre le choix du joueur
+const jouer = (choixdujoueur) => {
+    //  creation d'un nombre aleatoire de 0 a 2 dans le tableau
+    const choixbot = choix[Math.floor(Math.random() * 3)];
+    // si les deux joueurs font le meme choix c'est une egalite
+    if (choixdujoueur === choixbot) {
+        return (resultat([choixdujoueur, choixbot, "égalité"]));
+    } 
+    // si le joueur gagne
+    else if (
+        (choixdujoueur === "ciseaux" && choixbot === "papier") ||
+        (choixdujoueur === "papier" && choixbot === "pierre") ||
+        (choixdujoueur === "pierre" && choixbot === "ciseaux")
+    ) {
+        return (resultat([choixdujoueur, choixbot, "Bravo ! Vous avez gagné"]));
+    } 
+    // si le bot gagne
+    else {
+        return (resultat([choixdujoueur, choixbot, "bot a gagné"]));
+    }
+}
+
+*/
